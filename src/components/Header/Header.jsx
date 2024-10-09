03f9259c8d9df19fcec629e925ff876127a6d56d
uelel <48937159+uelel@users.noreply.github.com>
2024-10-09 15:01:50 +0200

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,16 @@ class Header extends React.Component {
   render() {
     return (
       <div className="w-full h-24 inline-flex justify-start items-center p-5 bg-white">
-        <h1>Header</h1>
+        <img src="logo.png" className="w-32" alt="Logo"/>
+        <nav className="flex items-center whitespace-nowrap overflow-x-auto overflow-y-hidden">
+          <div className="hidden md:flex items-center flex-shrink-1 pl-5 pr-32">
+            <div tabIndex="0" className="outline-none flex items-center group leading-tight ml-10 py-3 md:pt-5 md:pb-4 ml-not-first cursor-pointer select-none font-bold">
+              <div className="flex flex-shrink-0 items-center justify-center mr-3 rounded-full border-1 border-gray w-8 h-8 transition duration-200 border-none bg-primary text-white group-hover:bg-gray group-focus:bg-gray group-hover:text-black group-focus:text-black">
+              1
+              </div> <span>Materiál</span>
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }
